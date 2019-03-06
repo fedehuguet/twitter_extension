e43f7a589933032b22da839f60089fa1335b8907
fedehuguet <fede.huguet14@gmail.com>
2019-03-05 19:53:58 -0600

    Inicial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Twitter Chrome Extension
+One Paragraph of project description goes here
+
+## Getting Started
+### Installing
+
+A step by step series of examples that tell you how to get a development env running
+
+Say what the step will be
+
+```
+pip3 install -r requirements.txt
+```
+
+## Testing the API
+
+python yourpath/api_rest.py
---
+++ api/api_rest.py
@@ -0,0 +1,22 @@
+#import os
+from flask import Flask, request
+from flask_restful import Resource, Api
+from keys import consumer_key, consumer_secret, access_token, access_secret
+
+app = Flask(__name__)
+api = Api(app)
+
+todos = {}
+
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
