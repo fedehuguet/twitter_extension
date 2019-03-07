97b1e020a28b472f088faa1738818858ee0f6988
fedehuguet <fede.huguet14@gmail.com>
2019-03-06 19:06:49 -0600

    CAmbios

--- api/api_rest.py
+++ api/api_rest.py
@@ -1,28 +1,52 @@
 #import os
+import tweepy
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
 from flask import Flask, request
 from flask_restful import Resource, Api
 from keys import consumer_key, consumer_secret, access_token, access_secret
 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
 app = Flask(__name__)
 api = Api(app)
+api_tweepy = tweepy.API(auth)
 
-todos = {}
-
-class TodoSimple(Resource):
-    def get(self, id):
-        return {
-            'id': id,
-            'name':todos[id]
-            }
-
+class Search(Resource):
     def put(self, id):
-        todos[id] = request.form['data']
+        query = request.form['data']
+        twitter_stream.filter(track=[query])
         return {
             'id': id,
-            'name':todos[id]
+            'name':'Succesful query!'
             }
 
-api.add_resource(TodoSimple, '/<string:id>')
+class MyListener(StreamListener):
+    def __init__(self):
+        self.counter = 0
+        self.limit = 10
+    
+    def on_data(self, data):
+        try:
+            with open('tweet_history.json', 'a') as f:
+                f.write(data)
+                self.counter += 1
+                if self.counter < self.limit:
+                    return True
+                else:
+                    twitter_stream.disconnect()
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+ 
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+twitter_stream = Stream(auth, MyListener())
+api.add_resource(Search, '/<string:id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
